b7c527d7a8c1facbbdb1b5eabcd9d4b94414f51e
ex3ploiter <masoudhadi642924@gmail.com>
2023-02-05 13:17:00 +0330

    t

--- Deep_SVDD-Attack/Deep-SVDD-PyTorch-master/src/optim/Attack.py
+++ Deep_SVDD-Attack/Deep-SVDD-PyTorch-master/src/optim/Attack.py
@@ -2,16 +2,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-
-
-upper_limit, lower_limit = 1,0
-
-
 def fgsm(model, inputs, c, epsilon,objective,R):
     """ Construct FGSM adversarial examples on the examples X"""
-    delta = torch.zeros_like(inputs, requires_grad=True).to(device)
-    delta.requires_grad = True
+    delta = torch.zeros_like(inputs, requires_grad=True)
+    
     
     outputs = model(inputs+delta)
     
@@ -26,19 +20,13 @@ def fgsm(model, inputs, c, epsilon,objective,R):
     # return inputs+epsilon * delta.grad.detach().sign()
     return epsilon * delta.grad.detach().sign()
 
-def clamp(X, lower_limit, upper_limit):
-    return torch.max(torch.min(X, upper_limit), lower_limit)
 
-def pgd(model, inputs, c, epsilon, alpha, num_iter,objective,R,norm='l_inf'):
+def pgd(model, inputs, c, epsilon, alpha, num_iter,objective,R):
 
-    
-    delta = torch.zeros_like(inputs).to(device)
-    if norm == "l_inf":
-            delta.uniform_(-epsilon, epsilon)
-    delta = clamp(delta, lower_limit-inputs, upper_limit-inputs)
-    delta.requires_grad = True
-    
-    for _ in range(num_iter):
+    delta = torch.zeros_like(inputs, requires_grad=True)
+    for t in range(num_iter):
+        
+
         outputs = model(inputs+delta)
         dist = torch.sum((outputs - c) ** 2, dim=1)
         if objective == 'soft-boundary':
@@ -47,10 +35,8 @@ def pgd(model, inputs, c, epsilon, alpha, num_iter,objective,R,norm='l_inf'):
             scores = dist
         scores.backward()        
         
-        if norm == "l_inf":
-                delta.data = torch.clamp(delta + alpha * torch.sign(delta.grad.data), min=-epsilon, max=epsilon)
-        delta.data = clamp(delta, lower_limit - inputs, upper_limit - inputs)
         
+        
+        delta.data = (delta + inputs.shape[0]*alpha*delta.grad.data).clamp(-epsilon,epsilon)
         delta.grad.zero_()
-    
     return delta.detach()
